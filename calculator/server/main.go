fdb82723147b53a812bb55584f2a3e5a16fcdd3d
Avery <kcih4518@gmail.com>
2022-08-28 22:11:50 +0800

    feat: create calculator grpc server on localhost:50051

---
+++ calculator/server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/Kcih4518/grpc-golan/calculator/proto"
+	"google.golang.org/grpc"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.CalculatorServiceServer
+}
+
+func main() {
+	lst, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v\n", err)
+	}
+
+	log.Printf("Listening on %s\n", addr)
+
+	grpcServer := grpc.NewServer()
+
+	// pb.RegisterCalculatorServiceServer(grpcServer, &Server{})
+
+	if err = grpcServer.Serve(lst); err != nil {
+		log.Fatalf("Failed to serve: %v\n", err)
+	}
+}
