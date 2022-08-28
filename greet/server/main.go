d27716e2cbf2329fa20badf16ca014a4b1b5db49
Avery Yang(楊長珈) <avery_yang@asus.com>
2022-08-28 17:01:36 +0800

    feat: create grpc serser on localhost:50051

---
+++ greet/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/Kcih4518/grpc-golan/greet/proto"
+	"google.golang.org/grpc"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lst, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v\n", err)
+	}
+
+	log.Printf("Listening on %s\n", addr)
+
+	grpcServer := grpc.NewServer()
+
+	if err = grpcServer.Serve(lst); err != nil {
+		log.Fatalf("Failed to serve: %v\n", err)
+	}
+}
