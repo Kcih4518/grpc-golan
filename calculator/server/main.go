03ae3e05124e54619770c8684b12d1e23f6e5919
Avery <kcih4518@gmail.com>
2022-09-01 12:38:15 +0800

    feat: modify server to use tls

--- calculator/server/main.go
+++ calculator/server/main.go
@@ -6,6 +6,7 @@ import (
 
 	pb "github.com/Kcih4518/grpc-golan/calculator/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 var addr string = "0.0.0.0:50051"
@@ -19,10 +20,23 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to listen: %v\n", err)
 	}
-
+	defer lst.Close()
 	log.Printf("Listening on %s\n", addr)
 
-	grpcServer := grpc.NewServer()
+	opts := []grpc.ServerOption{}
+	tls := true
+
+	if tls {
+		certFile := "ssl/server.crt"
+		keyFile := "ssl/server.pem"
+		creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+		if err != nil {
+			log.Fatalf("Failed loading certificates: %v\n", err)
+		}
+		opts = append(opts, grpc.Creds(creds))
+	}
+
+	grpcServer := grpc.NewServer(opts...)
 
 	pb.RegisterCalculatorServiceServer(grpcServer, &Server{})
 
