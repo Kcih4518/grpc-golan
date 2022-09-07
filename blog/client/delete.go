4836ecdc61ef6d1a1f08628db5e93ed3966bad0d
Avery <kcih4518@gmail.com>
2022-09-08 00:37:55 +0800

    feat: add deleteBlog client to send del Blog req

---
+++ blog/client/delete.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/Kcih4518/grpc-golan/blog/proto"
+)
+
+func deleteBlog(c pb.BlogServiceClient, id string) {
+	log.Println("---deleteBlog was invoked---")
+	_, err := c.DeleteBlog(context.Background(), &pb.BlogId{Id: id})
+	if err != nil {
+		log.Fatalf("Error happened while deleting: %v\n", err)
+	}
+
+	log.Println("Blog was deleted")
+}
